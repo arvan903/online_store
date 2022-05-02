5919736c2d0349814e08d7c6d2fd96193937f3d4
arvan903 <arvan903@hotmail.com>
2022-05-02 12:12:39 +0430

    in progress

--- lib/routes/mainhome.dart
+++ lib/routes/mainhome.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../widgets/customactionbar.dart';
 
 class MainHome extends StatelessWidget {
-  const MainHome({Key? key}) : super(key: key);
+  MainHome({Key? key}) : super(key: key);
+
+  final Stream<QuerySnapshot> _product =
+      FirebaseFirestore.instance.collection('products').snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Stack(
         children: [
+          StreamBuilder(
+            stream: _product,
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              //if connection has error
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error ${snapshot.error}'),
+                );
+              }
+              // if connection is loading ;
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return Center(
+                child: Container(
+                  child: Text('ProductName : Shoes'),
+                ),
+              );
+            },
+          ),
           Center(child: Text('HomePage')),
           CAB(
             number: '0',
