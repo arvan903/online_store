da41e4111097ab42bbda16593e828a9b8b79b4f7
arvan903 <arvan903@hotmail.com>
2022-05-13 22:05:59 +0430

    in progress

--- lib/routes/mainhome.dart
+++ lib/routes/mainhome.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:online_store/routes/const.dart';
 import '../widgets/customactionbar.dart';
 
 class MainHome extends StatelessWidget {
@@ -30,12 +31,64 @@ class MainHome extends StatelessWidget {
               }
               return Center(
                 child: Container(
-                  child: Text('ProductName : Shoes'),
+                  child: ListView(
+                      children:
+                          snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                        document.data()! as Map<String, dynamic>;
+                    return Center(
+                        child: Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 13.0, horizontal: 23.0),
+                          width: double.infinity,
+                          height: 500.0,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12)),
+                          child: Center(
+                              child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(12),
+                                  child: Image.network(
+                                    data['image'][3],
+                                    fit: BoxFit.cover,
+                                  ))),
+                        ),
+                        Positioned(
+                          bottom: 0.0,
+                          left: 0.0,
+                          right: 0.0,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(3.0),
+                                ),
+                                child: Text(
+                                  data['name'],
+                                  style: Constans.textStyleOne,
+                                ),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(3.0),
+                                ),
+                                child: Text(data['price'].toString(),
+                                    style: Constans.textStyleOne),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ));
+                  }).toList()),
                 ),
               );
             },
           ),
-          Center(child: Text('HomePage')),
           CAB(
             number: '0',
             title: 'Home',
